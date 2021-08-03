ef04fe93c539970c1517823eb64e3dc8bb6515a0
lucasdrc <lucasdrc@id.uff.br>
2021-08-03 19:47:55 -0300

    Desenvolvimento das features base de territórios

--- Area2D.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-func _on_Area2D_mouse_entered():
-	pass
-	#modulate.a = 0.3
-
-func _on_Area2D_mouse_exited():
-	#modulate.a = 0
-	pass
---
+++ Scripts/Main.gd
@@ -0,0 +1,51 @@
+extends Node2D
+
+enum GAME_STATES {INITIAL, PLACING_TERRITORIES, ATTACKING, MOVING_TERRITORIES}
+onready var current_state = GAME_STATES.INITIAL
+onready var player_scene = preload("res://Scenes/Player.tscn")
+onready var PLAYER_COUNT = 3
+onready var START_INFANTARY_COUNT = 35
+onready var current_player = 0
+onready var territories = get_tree().get_nodes_in_group("territories")
+onready var selected_territory = null
+onready var players = []
+
+func _ready():
+	randomize()
+	_start_territories()
+	current_player = 0
+
+func _process(delta):
+	$Info.text = "Current state: " + str(GAME_STATES.keys()[current_state]) + '\n'
+	$Info.text += "Current player: " + get_current_player().color.name + '\n'
+	$Info.text += "Infantary remaining: " + str(players[current_player].infantary_count)
+
+func _start_territories():
+	for i in range(PLAYER_COUNT):
+		var instance = player_scene.instance()
+		instance.infantary_count = 35
+		instance.color = instance.COLORS[i]
+		instance.name = "Player " + str(i + 1)
+		players.append(instance)
+		add_child(instance)
+	territories.shuffle()
+	for i in range(territories.size()):
+		current_player = i%PLAYER_COUNT
+		place_infantary(territories[i])
+		territories[i].player_owner_index = current_player
+
+func place_infantary(territory):
+	if current_state == GAME_STATES.INITIAL:
+		if(players[current_player].infantary_count):
+			players[current_player].infantary_count -= 1
+			territory.infantary_count += 1
+		else:
+			current_player += 1
+			current_player = current_player%PLAYER_COUNT
+
+func get_current_player():
+	return players[current_player]
+
+func get_player(index):
+	return players[index]
+
