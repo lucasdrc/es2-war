2a42f5b7dd0a622bd3689a12071994a98a56d586
Patricia <patriciamrc96@gmail.com>
2021-08-11 22:43:22 -0300

    finalizando a tela de seleção de jogadores

--- Scripts/PlayerSelectionScreen.gd
+++ Scripts/PlayerSelectionScreen.gd
@@ -14,3 +14,9 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+func _on_BackButton_pressed():
+	get_tree().change_scene("res://Scenes/TitleScreen.tscn")
+
+
+func _on_StartGameButton_pressed():
+	pass # Replace with function body.
