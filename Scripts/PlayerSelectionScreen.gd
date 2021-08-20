93e2d0ae2683b1f157246eb0891889a070fb39fc
Patricia <patriciamrc96@gmail.com>
2021-08-19 22:15:26 -0300

    #152 Corrigir funcionamento dos botões de seleção de jogadores

--- Scripts/PlayerSelectionScreen.gd
+++ Scripts/PlayerSelectionScreen.gd
@@ -8,26 +8,16 @@ var players = [
 	PLAYER_STATE.PLAYER, #BLUE
 	PLAYER_STATE.PLAYER, #BLACK
 	PLAYER_STATE.PLAYER, #GREEN
-	PLAYER_STATE.DISABLED, #ORANGE
-	PLAYER_STATE.DISABLED, #YELLOW
-	PLAYER_STATE.DISABLED, #WHITE
+	PLAYER_STATE.PLAYER, #ORANGE
+	PLAYER_STATE.PLAYER, #YELLOW
+	PLAYER_STATE.PLAYER, #WHITE
 ]
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 func _on_BackButton_pressed():
 	get_tree().change_scene("res://Scenes/TitleScreen.tscn")
-
 
 func _on_StartGameButton_pressed():
 	var count = 0
@@ -40,3 +30,4 @@ func _on_StartGameButton_pressed():
 func changePlayerState(index, state):
 	players[index] = state
 	print(players)
+	
