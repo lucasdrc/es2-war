248eb9f06e8e4d7b71428d9b950a02da43a76b1f
Patricia <patriciamrc96@gmail.com>
2021-08-23 22:57:36 -0300

    Criação das telas de dados e seleção de tropas

---
+++ Scripts/Dice.gd
@@ -0,0 +1,28 @@
+extends Node
+
+func _ready():
+	pass # Replace with function body.
+
+func showOneDice():
+	$One.show()
+	pass
+
+func showTwoDice():
+	$Two.show()
+	pass
+	
+func showThreeDice():
+	$Three.show()
+	pass
+	
+func showFourDice():
+	$Four.show()
+	pass
+	
+func showFiveDice():
+	$Five.show()
+	pass
+	
+func showSixDice():
+	$Six.show()
+	pass
