36dbeecfa3fe6ca9340b1f62d3d1adbccd377fa2
JoaoLopez <joaolopez@id.uff.br>
2021-09-09 13:45:29 -0300

    Removendo mensagens de DEBUG  do script TitleScreen.gd

--- Scripts/TitleScreen.gd
+++ Scripts/TitleScreen.gd
@@ -6,7 +6,6 @@ func _ready():
 func _on_CloseButton_pressed():
 	get_tree().quit();
 	Log.add_log_msg("Program finished.")
-	print(Log.log_msgs)
 
 func _on_NewGameButton_pressed():
 	get_tree().change_scene("res://Scenes/PlayerSelectionScreen.tscn")
