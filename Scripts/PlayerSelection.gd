e1572bf43adcdc032422eb55846e63bee0dd40e2
JoaoLopez <joaolopez@id.uff.br>
2021-09-09 13:24:04 -0300

    Limpando o script PlayerSelection.gd

--- Scripts/PlayerSelection.gd
+++ Scripts/PlayerSelection.gd
@@ -4,45 +4,23 @@ enum PLAYER_STATE {PLAYER, IA, DISABLED}
 
 onready var playerController : PlayerSelectionScreen = get_tree().root.find_node("PlayerSelectionScreen", true, false) as PlayerSelectionScreen
 
-var state = PLAYER_STATE.PLAYER
-
 func _ready():
 	pass
 
-func controlsButtons():
-	match state:
-		PLAYER_STATE.PLAYER:
-			$Player/Color/GridContainer/Buttons/PlayerSelectionBox/Player/ColorRect/ButtonsPlayerSelection/IAButton.pressed = false
-			$Player/Color/GridContainer/Buttons/PlayerSelectionBox/Player/ColorRect/ButtonsPlayerSelection/DesativadoButton.pressed = false
-		
-		PLAYER_STATE.IA:
-			$Player/Color/GridContainer/Buttons/PlayerSelectionBox/Player/ColorRect/ButtonsPlayerSelection/PlayerSelectionButton.pressed = false
-			$Player/Color/GridContainer/Buttons/PlayerSelectionBox/Player/ColorRect/ButtonsPlayerSelection/DesativadoButton.pressed = false
-		
-		PLAYER_STATE.DISABLED:
-			$Player/Color/GridContainer/Buttons/PlayerSelectionBox/Player/ColorRect/ButtonsPlayerSelection/IAButton.pressed = false
-			$Player/Color/GridContainer/Buttons/PlayerSelectionBox/Player/ColorRect/ButtonsPlayerSelection/PlayerSelectionButton.pressed = false
-
 func _on_PlayerSelectionButton_pressed():
-	if state == PLAYER_STATE.PLAYER:
-		$Player/Color/GridContainer/Buttons/PlayerSelectionBox/Player/ColorRect/ButtonsPlayerSelection/PlayerSelectionButton.pressed = true
-		pass
-	state = PLAYER_STATE.PLAYER
-	playerController.changePlayerState(get_index(), state)
-	controlsButtons()
-
-func _on_DisabledSelectionButton_pressed():
-	if state == PLAYER_STATE.DISABLED:
-		$Player/Color/GridContainer/Buttons/PlayerSelectionBox/Player/ColorRect/ButtonsPlayerSelection/DesativadoButton.pressed = true
-		pass
-	state = PLAYER_STATE.DISABLED
-	playerController.changePlayerState(get_index(), state)
-	controlsButtons()
+	$Player/Color/GridContainer/Buttons/PlayerSelectionBox/Player/ColorRect/ButtonsPlayerSelection/PlayerSelectionButton.pressed = true
+	$Player/Color/GridContainer/Buttons/PlayerSelectionBox/Player/ColorRect/ButtonsPlayerSelection/IAButton.pressed = false
+	$Player/Color/GridContainer/Buttons/PlayerSelectionBox/Player/ColorRect/ButtonsPlayerSelection/DesativadoButton.pressed = false
+	playerController.changePlayerState(get_index(), PLAYER_STATE.PLAYER)
 
 func _on_IaSelectionButton_pressed():
-	if state == PLAYER_STATE.IA:
-		$Player/Color/GridContainer/Buttons/PlayerSelectionBox/Player/ColorRect/ButtonsPlayerSelection/IAButton.pressed = true
-		pass
-	state = PLAYER_STATE.IA
-	playerController.changePlayerState(get_index(), state)
-	controlsButtons()
+	$Player/Color/GridContainer/Buttons/PlayerSelectionBox/Player/ColorRect/ButtonsPlayerSelection/PlayerSelectionButton.pressed = false
+	$Player/Color/GridContainer/Buttons/PlayerSelectionBox/Player/ColorRect/ButtonsPlayerSelection/IAButton.pressed = true
+	$Player/Color/GridContainer/Buttons/PlayerSelectionBox/Player/ColorRect/ButtonsPlayerSelection/DesativadoButton.pressed = false
+	playerController.changePlayerState(get_index(), PLAYER_STATE.IA)
+
+func _on_DisabledSelectionButton_pressed():
+	$Player/Color/GridContainer/Buttons/PlayerSelectionBox/Player/ColorRect/ButtonsPlayerSelection/PlayerSelectionButton.pressed = false
+	$Player/Color/GridContainer/Buttons/PlayerSelectionBox/Player/ColorRect/ButtonsPlayerSelection/IAButton.pressed = false
+	$Player/Color/GridContainer/Buttons/PlayerSelectionBox/Player/ColorRect/ButtonsPlayerSelection/DesativadoButton.pressed = true
+	playerController.changePlayerState(get_index(), PLAYER_STATE.DISABLED)
