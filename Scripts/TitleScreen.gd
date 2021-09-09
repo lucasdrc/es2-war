fa2bf33e415c249a34509c709e2232ead23e26fb
JoaoLopez <joaolopez@id.uff.br>
2021-09-09 12:01:35 -0300

    Adicionando mensagens de log à TitleScreen

--- Scripts/TitleScreen.gd
+++ Scripts/TitleScreen.gd
@@ -1,10 +1,12 @@
 extends Node
 
 func _ready():
-	pass 
+	Log.add_log_msg("Title Screen lodaded.")
 
 func _on_CloseButton_pressed():
 	get_tree().quit();
+	Log.add_log_msg("Program finished.")
+	print(Log.log_msgs)
 
 func _on_NewGameButton_pressed():
 	get_tree().change_scene("res://Scenes/PlayerSelectionScreen.tscn")
