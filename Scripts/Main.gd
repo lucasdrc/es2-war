420590e38ca75668aafae183cb1dd9a3afffe274
JoaoLopez <joaolopez@id.uff.br>
2021-09-09 12:48:23 -0300

    Adicionando mensagens de log ao script Main.gd

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -14,10 +14,14 @@ onready var selected_territory = null
 onready var players = []
 
 func _ready():
+	Log.add_log_msg("New game started.")
+	Log.add_log_msg("Main Scene loaded.")
 	randomize()
-	_instantiating_players()
+	_instantiating_players()	
 	_start_territories()
 	current_player = 0
+	Log.add_log_msg("New current player: {0} ({1}).".format([players[current_player].name,
+															 players[current_player].color.name]))
 
 func _process(delta):
 	$Info.text = "CURRENT PLAYER: " + get_current_player().color.name + '\n'
@@ -33,6 +37,8 @@ func _current_player_movement_done():
 
 func update_current_player():
 	current_player = (current_player + 1)%PLAYER_COUNT
+	Log.add_log_msg("Current player updated: {0} ({1}).".format([players[current_player].name,
+															 players[current_player].color.name]))
 	
 func _update_game_state():
 	if(current_state == GameInfo.GAME_STATES.INITIAL and _current_player_movement_done()):
@@ -64,6 +70,7 @@ func _instantiating_players():
 		new_player.name = "Player " + str(i + 1)
 		players.append(new_player)
 		$Players.add_child(new_player)
+	Log.add_log_msg("Players instantiated.")
 
 func _start_territories():
 	territories.shuffle()
@@ -71,11 +78,13 @@ func _start_territories():
 		current_player = i%PLAYER_COUNT
 		place_infantary(territories[i])
 		territories[i].player_owner_index = current_player
+	Log.add_log_msg("Territories assigned to players.")
 
 func place_infantary(territory):
 	if(players[current_player].infantary_count):
 		players[current_player].infantary_count -= 1
 		territory.infantary_count += 1
+		Log.add_log_msg("+1 infantary added to %s." % territory.name)
 
 func attack_territory(attacking_territory: Territory, defending_territory: Territory):
 	if(attacking_territory.infantary_count == 1):
@@ -86,6 +95,9 @@ func attack_territory(attacking_territory: Territory, defending_territory: Terri
 		defending_territory.infantary_count -= 1
 		if(not defending_territory.infantary_count):
 			defending_territory.player_owner_index = current_player
+			Log.add_log_msg("{0} ({1}) conquered {2}.".format([players[current_player].name,
+															   players[current_player].color.name,
+															   defending_territory.name]))
 			var dialog = move_infantary_dialog.instance()
 			dialog.attacker_territory = attacking_territory
 			dialog.defeated_territory = defending_territory
@@ -98,9 +110,11 @@ func move_infantary_to_defeated_territory(attacker: Territory, defeated: Territo
 	if(attacker.infantary_count + 1 > amount):
 		attacker.infantary_count -= amount
 		defeated.infantary_count += amount
+		Log.add_log_msg("+%d infatary(ies) moved to %s." % [amount, defeated.name])
 
 func change_game_state(new_state):
 	current_state = new_state
+	Log.add_log_msg("Game state updated: %s" % GameInfo.GAME_STATES.keys()[current_state])
 	if new_state == GameInfo.GAME_STATES.PLACING_TERRITORIES:
 		(players[current_player] as Player).receive_infantary()
 		
@@ -113,7 +127,7 @@ func get_player(index) -> Player:
 func get_current_state():
 	return current_state
 
-func _on_LogButton_pressed():	
+func _on_LogButton_pressed():
 	if(get_node_or_null("LogDialog") == null):
 		var log_dialog = log_dialog_scene.instance()
 		add_child(log_dialog)
