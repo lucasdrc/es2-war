00146f821856b86e862870d175d73e778cc3b589
JoaoLopez <joaolopez@id.uff.br>
2021-09-09 13:29:40 -0300

    Unificando modo de utilização do enum PLAYER_STATE

--- Scripts/PlayerSelection.gd
+++ Scripts/PlayerSelection.gd
@@ -1,7 +1,5 @@
 extends Control
 
-enum PLAYER_STATE {PLAYER, IA, DISABLED}
-
 onready var playerController : PlayerSelectionScreen = get_tree().root.find_node("PlayerSelectionScreen", true, false) as PlayerSelectionScreen
 
 func _ready():
@@ -11,16 +9,16 @@ func _on_PlayerSelectionButton_pressed():
 	$Player/Color/GridContainer/Buttons/PlayerSelectionBox/Player/ColorRect/ButtonsPlayerSelection/PlayerSelectionButton.pressed = true
 	$Player/Color/GridContainer/Buttons/PlayerSelectionBox/Player/ColorRect/ButtonsPlayerSelection/IAButton.pressed = false
 	$Player/Color/GridContainer/Buttons/PlayerSelectionBox/Player/ColorRect/ButtonsPlayerSelection/DesativadoButton.pressed = false
-	playerController.changePlayerState(get_index(), PLAYER_STATE.PLAYER)
+	playerController.changePlayerState(get_index(), GameInfo.PLAYER_STATE.PLAYER)
 
 func _on_IaSelectionButton_pressed():
 	$Player/Color/GridContainer/Buttons/PlayerSelectionBox/Player/ColorRect/ButtonsPlayerSelection/PlayerSelectionButton.pressed = false
 	$Player/Color/GridContainer/Buttons/PlayerSelectionBox/Player/ColorRect/ButtonsPlayerSelection/IAButton.pressed = true
 	$Player/Color/GridContainer/Buttons/PlayerSelectionBox/Player/ColorRect/ButtonsPlayerSelection/DesativadoButton.pressed = false
-	playerController.changePlayerState(get_index(), PLAYER_STATE.IA)
+	playerController.changePlayerState(get_index(), GameInfo.PLAYER_STATE.IA)
 
 func _on_DisabledSelectionButton_pressed():
 	$Player/Color/GridContainer/Buttons/PlayerSelectionBox/Player/ColorRect/ButtonsPlayerSelection/PlayerSelectionButton.pressed = false
 	$Player/Color/GridContainer/Buttons/PlayerSelectionBox/Player/ColorRect/ButtonsPlayerSelection/IAButton.pressed = false
 	$Player/Color/GridContainer/Buttons/PlayerSelectionBox/Player/ColorRect/ButtonsPlayerSelection/DesativadoButton.pressed = true
-	playerController.changePlayerState(get_index(), PLAYER_STATE.DISABLED)
+	playerController.changePlayerState(get_index(), GameInfo.PLAYER_STATE.DISABLED)
